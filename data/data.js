68c121b69cb5990298265c69de00fa663aedf72c
GGCristo <cgg.code@gmail.com>
2022-05-19 23:42:27 +0100

    Functional

--- data/data.js
+++ data/data.js
@@ -1,4 +1,5 @@
 import inquirer from 'inquirer';
+import chalk from 'chalk';
 import tmp from "tmp";
 
 /** Load configuration */
@@ -21,7 +22,6 @@ async function setMetadata(data) {
     }
   ]
   let result = await inquirer.prompt(questions).then(answers => answers);
-  console.log("result: ", result);
   questions = [
     {
       type: 'list',
@@ -64,27 +64,35 @@ const query = (org, node) => `
 // https://majiehong.com/post/2021-03-08_fzf_jq_play_locally/
 async function fill(localData, metaData) {
   const util = await utility;
-  let { data: { organization: { membersWithRole: { nodes: remoteData } } } } = JSON.parse(util.executeQuery(query(config.defaultOrg, metaData.desiredData)));
+  const { data: { organization: { membersWithRole: { nodes: remoteData } } } } = JSON.parse(util.executeQuery(query(config.defaultOrg, metaData.desiredData)));
   const oneLine = remoteData.map(data => data.login).join('\n');
   const tmpFile = tmp.tmpNameSync();
   fs.writeFileSync(tmpFile, JSON.stringify(remoteData, null, 2));
   for (const [index, member] of localData.entries()) {
     const prompt = `Member: ${member[metaData.name]} ID: ${member[metaData.id]} > `;
-    let command = `echo "${oneLine}"` + `| fzf --ansi --prompt='${prompt}' --preview ` + `"cat ${tmpFile} | jq '.[] | select(.login==\\"{}\\")'"`;
-    let result = util.runCommand(command).replace(/\s/g, ''); // TODO why do I need to do this?
-    for (const [index, data] of remoteData.entries()) {
-      if (data.login === result) {
+    let command = `echo "${oneLine}"` + `| fzf -m --ansi --prompt='${prompt}' --preview ` + `"cat ${tmpFile} | jq -C '.[] | select(.login==\\"{}\\")'"`;
+    const selectedLogin = util.runCommand(command).replace(/\s/g, ''); // TODO handle signal C-D C-C
+    const selectedData = JSON.parse(util.runCommand(`cat ${tmpFile} | jq '.[] | select(.login==\"${selectedLogin}\")'`, true));
+    for (const field in selectedData) {
+      if (field in localData[index]) {
+        // TODO warning. make an alias to don't overwrite original data
       }
+      localData[index][field] = selectedData[field];
     }
-    console.log("localData: ", localData);
   }
+  return localData;
 }
 
 // export const chooseOrgName = "gh api --paginate /user/memberships/orgs  --jq '.[].organization.login' | fzf  --prompt='Choose an organization> ' --layout=reverse --border";
-export default async function data(file) {
+export default async function data(file, options) {
   if (!config.defaultOrg) {
     console.error("Please set an organization as default")
     return;
+  }
+  let outputFile = options.output;
+  if (!options.output) {
+    console.log(chalk.yellow("Warning you are not using --output. The input file will be overwrite"));
+    outputFile = file;
   }
   const fileString = fs.readFileSync(file, { encoding: "utf8" })
   const data = JSON.parse(fileString);
@@ -93,7 +101,8 @@ export default async function data(file) {
   // console.log("metaData: ", metaData);
   if (check(data, metaData))
     return;
-  fill(data, metaData);
+  const result = await fill(data, metaData);
+  fs.writeFileSync(outputFile, JSON.stringify(result, null, 2));
   console.log("Done!!!");
 }
 
--- gh-edu-data.js
+++ gh-edu-data.js
@@ -3,8 +3,9 @@ import data from './data/data.js'
 
 program
   .description("Save some data about your users")
-  .argument("<file>")
-  .action((file) => {
-    data(file);
+  .argument("<inputFile>")
+  .option("-o, --output <outputFile>", "File to write the resulting data")
+  .action((file, options) => { // TODO add quit option
+    data(file, options);
   })
 program.parse();
